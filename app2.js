486cb249b2b831937020c6c9dfe20419554fd67a
Funda SARIKAYA <fundasarikaya58@hotmail.com>
2019-03-31 20:41:48 +0300

    initial commit

---
+++ app2.js
@@ -0,0 +1,30 @@
+//ders88
+//Fetch API Kullanımı
+//ajax yerine kullanılabilece asenksron veri alıp verme
+//ajaxtan daha kolaydır
+function getTextFile(){//text dosyasından veri alma
+    fetch("example.txt")
+    .then(response=>response.text())
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err));
+}
+//getTextFile();
+
+function getJsonFile(){ //json dosyasından veri alma
+    fetch("example.json")
+    .then(response=>response.json())
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err));
+
+}
+//getJsonFile();
+function getExternalAPI(){
+    fetch("https://api.exchangeratesapi.io/latest")
+    .then(response=>response.json())
+   // .then(data=>console.log(data))
+    .then(data=>{
+        console.log(data.rates.TRY);
+    })
+    .catch(err=>console.log(err));
+}
+getExternalAPI();
